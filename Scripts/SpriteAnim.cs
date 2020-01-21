751ceab0938a1cdc88158760e44a223e886de9e7
timfors <tkhabibullin2014@litsey2.ru>
2020-01-21 16:45:04 +0300

    correected Suspencion

---
+++ Scripts/SpriteAnim.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnim : MonoBehaviour
+{   
+    [SerializeField]
+    Animator allAnims;
+
+
+
+    int currentAnim;
+    public float framesPerSecond { get; set; }
+    public bool isPlaying { get; private set; }
+    private void Start()
+    {
+        allAnims = GetComponent<Animator>();
+        framesPerSecond = 25;
+        isPlaying = false;
+    }
+
+    public void SetState(string name, int index)
+    {
+        try
+        {
+            framesPerSecond = allAnims.GetCurrentAnimatorClipInfo(0)[0].clip.frameRate;
+            allAnims.Play(name, 0, index / framesPerSecond);
+            allAnims.speed = 0;
+        } catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    IEnumerator Animate(string name, int beginFrame, int endFrame)
+    {
+        isPlaying = true;
+        allAnims.speed = 1;
+        while (beginFrame <= endFrame)
+        {
+            if (endFrame - beginFrame >= 0)
+            {
+                SetState(name, beginFrame++);
+            } else if (endFrame - beginFrame <= 0)
+            {
+                SetState(name, beginFrame--);
+            }
+            yield return new WaitForSeconds(1 / framesPerSecond);
+        }
+        isPlaying = false;
+    }
+
+    public void AnimateFromIndex(string name, int index)
+    {
+        StopAllCoroutines();
+        StartCoroutine(Animate(name, index, (int)(allAnims.GetCurrentAnimatorStateInfo(0).normalizedTime / framesPerSecond)));
+    }
+
+    public void AnimateFromTo(string name, int beginFrame, int endFrame)
+    {
+        StopAllCoroutines();
+        StartCoroutine(Animate(name, beginFrame, endFrame));
+    }
+}
---
+++ Scripts/Suspencion.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suspencion : MonoBehaviour
+{
+    [SerializeField]
+    AnimInfo[] animationsInfo;
+    SpriteAnim anim;
+    int currentAnim;
+    int currentAnimState = 0;
+    bool isVertical = false;
+    bool isHorizontal = false;
+    (float x, float y) maxDelta;
+
+
+    List<Vector3> touchPos = new List<Vector3>(){};
+    
+    void Start()
+    {
+        anim = GetComponent<SpriteAnim>();
+        maxDelta = (Screen.width / 4, Screen.height / 2);
+        StartCoroutine(Monitoring());
+    }
+
+    IEnumerator Monitoring()
+    {
+        while (true)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                touchPos.Add(touch.position);
+
+                Vector3 firstPos = touchPos[0];
+                Vector3 lastPos = touchPos[touchPos.Count - 1];
+
+                float deltaX = lastPos.x - firstPos.x;
+
+                float deltaY = lastPos.y - firstPos.y;
+
+                
+                float distancePerFrame;
+                if (Mathf.Abs(deltaX) > Mathf.Abs(deltaY) && !isVertical && !anim.isPlaying)
+                {
+                    isHorizontal = true;
+                    if (deltaX >= 0)
+                        currentAnim = 0;
+                    else
+                        currentAnim = 1;
+                    
+                    distancePerFrame = maxDelta.x / animationsInfo[currentAnim].limitFrame;
+                    currentAnimState = (int)(deltaX / distancePerFrame);
+                } else if (Mathf.Abs(lastPos.x - firstPos.x) < Mathf.Abs(lastPos.y - firstPos.y) && !isHorizontal && !anim.isPlaying)
+                {
+                    isVertical = true;
+                    if (deltaY > 0)
+                    {
+                        currentAnim = 2;
+                        distancePerFrame = maxDelta.y / animationsInfo[currentAnim].limitFrame;
+                        currentAnimState = (int)(deltaY / distancePerFrame);
+                    }
+                }
+                currentAnimState = currentAnimState < 0 ? -currentAnimState : currentAnimState;
+                currentAnimState = currentAnimState > animationsInfo[currentAnim].limitFrame ? animationsInfo[currentAnim].limitFrame : currentAnimState;
+                anim.SetState(animationsInfo[currentAnim].name, currentAnimState);
+            }
+
+            if (Input.touches.Length == 0 && currentAnimState != 0)
+            {
+                Debug.Log(touchPos.Count);
+                Debug.Log(currentAnimState);
+                anim.AnimateFromIndex(animationsInfo[currentAnim].name, currentAnimState + animationsInfo[currentAnim].limitFrame);
+                touchPos.Clear();
+                currentAnimState = 0;
+                isHorizontal = false;
+                isVertical = false;
+
+            }
+            
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    
+}
+
+[System.Serializable]
+public struct AnimInfo
+{
+    public int limitFrame;
+    public string name;    
+}
